c8216171ddaf6b946820689ff3b0fbe688ac55bc
Julien SCULTEUR <juliensculteur@gmail.com>
2023-09-05 16:47:25 +0200

    fix: - Changement de l'image - Modification du contraste du titre <h1> de l'accueil - Corrections indentation

--- components/Description.vue
+++ components/Description.vue
@@ -1,31 +1,34 @@
 <template>
-    <div class="description">
-        <img src="/qui_sommes_nous_1.png" alt="">
-        <h2 class="section_title">Qui sommes-nous ?</h2>
-        <p>
-            Une équipe française investie dans la création et la production de fictions sonores.<br>
-            Une association fondée par Aurélien Bernadas, qui s’entoure de nombreuses personnes bénévoles pour accompagner le projet et le faire grandir.
-        </p>
-    </div>
+  <div class="description">
+    <img src="/qui_sommes_nous_1.png" alt="" />
+    <h2 class="section_title">Qui sommes-nous ?</h2>
+    <p>
+      Une équipe française investie dans la création et la production de fictions
+      sonores.<br />
+      Une association fondée par Aurélien Bernadas, qui s’entoure de nombreuses personnes
+      bénévoles pour accompagner le projet et le faire grandir.
+    </p>
+  </div>
 </template>
 
 <style scoped>
 .description {
-    width: 100%;
-    margin-left: 5vw;
-    margin-right: 5vw;
-    margin-bottom: 5vw;
-    display: flex;
-    flex-direction: column;
+  width: 100%;
+  margin-left: 5vw;
+  margin-right: 5vw;
+  margin-bottom: 5vw;
+  display: flex;
+  flex-direction: column;
 }
 
 .description img {
-    width: 100%;
-    object-fit: contain;
+  width: 100%;
+  object-fit: contain;
+  border-radius: 0;
 }
 
 h2 {
-    margin-top: 2vw;
-    margin-bottom: 2vw;
+  margin-top: 2vw;
+  margin-bottom: 2vw;
 }
 </style>
--- pages/description.vue
+++ pages/description.vue
@@ -1,74 +1,76 @@
 <template>
-    <main class="main_box" role="main">
-        <img class="onde" src="/onde_deco.png" alt="">
-        <div class="description">
-            <img src="/qui_sommes_nous_2.png" alt="">
+  <main class="main_box" role="main">
+    <img class="onde" src="/onde_deco.png" alt="" />
+    <div class="description">
+      <img src="/qui_sommes_nous_2.png" alt="" />
 
-                <h1>Qui sommes-nous ?</h1>
+      <h1>Qui sommes-nous ?</h1>
 
-                <p>
-                    Une équipe française investie dans la création et la production de fictions sonores.<br>
-                    Une association fondée par Aurélien Bernadas, qui s’entoure de nombreuses personnes bénévoles pour accompagner le projet et le faire grandir.
-                </p>
+      <p>
+        Une équipe française investie dans la création et la production de fictions
+        sonores.<br />
+        Une association fondée par Aurélien Bernadas, qui s’entoure de nombreuses
+        personnes bénévoles pour accompagner le projet et le faire grandir.
+      </p>
 
-                <h2 class="blue_outline">Qu’est-ce qui nous fait vibrer ?</h2>
-                <p>
-                    Créer une œuvre accessible à tous, peu importe ses difficultés, son handicap, ses ressources financières…<br>
-                    Faire rêver, raconter des histoires, proposer un moment d’évasion.<br>
-                    Avancer en unissant nos énergies, nos compétences, nos différences, travailler en équipe, et être sur
-                    la même longueur d’onde
-                </p>
+      <h2 class="blue_outline">Qu’est-ce qui nous fait vibrer ?</h2>
+      <p>
+        Créer une œuvre accessible à tous, peu importe ses difficultés, son handicap, ses
+        ressources financières…<br />
+        Faire rêver, raconter des histoires, proposer un moment d’évasion.<br />
+        Avancer en unissant nos énergies, nos compétences, nos différences, travailler en
+        équipe, et être sur la même longueur d’onde
+      </p>
 
-                <h2 class="blue_outline">Pourquoi la fiction sonore ? </h2>
-                <p>
-                    Frequencies a choisi l’audio pour soutenir une idée forte: donner accès à tous aux fictions sonores
-                    et notamment aux personnes atteintes de déficiences visuelles.
-                </p>
+      <h2 class="blue_outline">Pourquoi la fiction sonore ?</h2>
+      <p>
+        Frequencies a choisi l’audio pour soutenir une idée forte: donner accès à tous aux
+        fictions sonores et notamment aux personnes atteintes de déficiences visuelles.
+      </p>
 
-                <h2 class="blue_outline">Une rencontre essentielle</h2>
-                <p>
-                    Aurélien a eu la chance d’entrer en contact avec Arthur, un jeune homme perdant la vue. Il lui dira pour
-                    commencer qu’il n’utilise pas l’audiodescription, ça le sort de l’histoire… Ça manque de vie !<br>
-                    Ils passeront deux ans à échanger sur comment faire une belle réalisation, proposer une histoire au plus
-                    grand nombre et adaptée aux personnes malvoyantes.<br>
-                    <br>
-                    C’est l’origine du projet !
-                </p>
-        </div>
-        <img class="onde" src="/onde_deco.png" alt="">
-    </main>
+      <h2 class="blue_outline">Une rencontre essentielle</h2>
+      <p>
+        Aurélien a eu la chance d’entrer en contact avec Arthur, un jeune homme perdant la
+        vue. Il lui dira pour commencer qu’il n’utilise pas l’audiodescription, ça le sort
+        de l’histoire… Ça manque de vie !<br />
+        Ils passeront deux ans à échanger sur comment faire une belle réalisation,
+        proposer une histoire au plus grand nombre et adaptée aux personnes
+        malvoyantes.<br />
+        <br />
+        C’est l’origine du projet !
+      </p>
+    </div>
+    <img class="onde" src="/onde_deco.png" alt="" />
+  </main>
 </template>
 
 <style scoped>
 .onde {
-    width: 100%;
-    object-fit: contain;
-    margin-bottom: 2vw;
+  width: 100%;
+  object-fit: contain;
+  margin-bottom: 2vw;
 }
 .description {
-    margin-left: 4vw;
-    margin-right: 4vw;
-    text-align: justify;
+  margin-left: 4vw;
+  margin-right: 4vw;
+  text-align: justify;
 }
 
 .description img {
-    float: left;
-    padding-right: 2vw;
+  float: left;
+  padding-right: 2vw;
 }
-
 </style>
 
 <script>
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent } from "@vue/composition-api";
 
 export default defineComponent({
-    setup() {
-        
-    },
-    head() {
-        return {
-            title: "Qui sommes-nous ?"
-        };
-    }
-})
+  setup() {},
+  head() {
+    return {
+      title: "Qui sommes-nous ?",
+    };
+  },
+});
 </script>
