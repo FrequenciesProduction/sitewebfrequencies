c45e2cb3f2851b22b085b0331d18cf452a8f42d1
RomainDelplace <romain.delplace@gmail.com>
2023-11-17 10:21:55 +0100

    Creation branch + modification carte

--- components/projectsMini/projectsMini.vue
+++ components/projectsMini/projectsMini.vue
@@ -1,5 +1,13 @@
 <template>
-    <div class="projects_box">
+    <div class="container">
+    <div v-for="emission in emissionsData" class="project">
+      <img :src="emission.itemObject.image">
+      <div>
+        <h2 class="black_outline">{{ emission.itemObject.title }}</h2>
+      </div>
+    </div>
+  </div>
+    <!-- <div class="projects_box">
 
         <div class="project project-blue">
             <h3>Frequencies</h3>
@@ -33,11 +41,17 @@
                           texte="J'écoute" />
         </div>
 
-    </div>
+    </div> -->
 </template>
 
-<script setup>
-import EmissionButton from "~/components/buttons/emissionButton.vue";
+
+<script>
+import axios from 'axios'
+import Parser from 'rss-parser'
+import {deleteEmissionUrl, emissionsBaseUrl} from '../../firebase';
+
+let parser = new Parser();
+
 </script>
 <style scoped>
 @import 'projectsMini.modules.scss';
