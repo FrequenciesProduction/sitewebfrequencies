2568e2d531f7a55c8496eaf32a85d88c7e02a34e
RomainDelplace <romain.delplace@gmail.com>
2023-11-21 09:32:46 +0100

    Affichage simple 5 derniers épisodes

--- components/projectsMini/projectsMini.vue
+++ components/projectsMini/projectsMini.vue
@@ -1,10 +1,9 @@
 <template>
-    <div class="container">
-    <div v-for="emission in emissionsData" class="project">
-      <!-- <img :src="emission.itemObject.image"> -->
-      <!-- <div>
-        <h2 class="black_outline">{{ emission.itemObject.title }}</h2>
-      </div> -->
+  <div class="container">
+    <div v-for="item in filteredItems" :key="item.title" class="project">
+      <div>
+        <h2 class="black_outline">{{ item.title }}</h2>
+      </div>
     </div>
   </div>
 </template>
@@ -18,10 +17,9 @@ import { deleteEmissionUrl, emissionsBaseUrl } from '../../firebase';
 export default {
   data() {
     return {
-      emissionsData: [], // Initialise ton tableau de données ici
+      emissionsData: [],
       parser: new Parser(),
-      // Autres données que tu utilises dans ton composant
-      // ...
+      filteredItems: [],
     }
   },
   async mounted() {
@@ -29,34 +27,36 @@ export default {
       const response = await axios.get(emissionsBaseUrl)
       const data = response.data;
 
-      // Itère sur chaque élément récupéré depuis Firebase
       for (const key in data) {
         if (Object.hasOwnProperty.call(data, key)) {
           const sourceValue = data[key].rss;
 
-          // Utilise Axios pour récupérer les données du flux RSS
           const res = await axios.get(sourceValue);
           const feedData = await this.parser.parseString(res.data);
 
-          // Ajoute les données du flux RSS dans emissionsData
           this.emissionsData.push(feedData);
         }
       }
 
-      // Fusion de toutes les sous-listes d'items en une seule liste
       const allItems = this.emissionsData.flatMap(rssFeed => rssFeed.items);
 
-      // Tri de la liste combinée par date (en supposant que la date est accessible via une propriété 'isoDate')
       allItems.sort((a, b) => {
         const dateA = new Date(a.isoDate);
         const dateB = new Date(b.isoDate);
-        return dateB - dateA; // Trie de la plus récente à la plus ancienne
+        return dateB - dateA;
+      });
+      
+      const filteredItems = allItems.filter(item => {
+        return !(
+          item.title.includes("Histoires courtes") || 
+          item.title.includes("Extrait")
+        );
       });
 
-      // Affichage des éléments triés par date
-      allItems.forEach(item => {
-        console.log(item.title, item.isoDate); // Affiche le titre et la date de chaque élément
+      filteredItems.forEach(item => {
+        console.log(item.title, item.isoDate);
       });
+      this.filteredItems = filteredItems.slice(0, 5);
     } catch (error) {
       console.error('Erreur lors de la récupération des données:', error)
     }
